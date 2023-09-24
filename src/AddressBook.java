2649803075dfc2c7aa6545d19a79066c274f3744
peter <petergrose@cmail.carleton.ca>
2023-09-24 13:10:47 -0400

    Updated the main method to print the name of the out the buddy that gets removed.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    private final ArrayList<BuddyInfo> buddyList;
 
+    private ArrayList<BuddyInfo> buddyList;
     public AddressBook() {
         buddyList = new ArrayList<>();
     }
@@ -13,16 +13,17 @@ public class AddressBook {
         }
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        if (buddy != null) {
-            buddyList.remove(buddy);
+    public BuddyInfo removeBuddy(int index) {
+        if (index >= 0 && index < buddyList.size()) {
+            return buddyList.remove(index);
         }
+        return null;
     }
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Peter","Carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        System.out.println(addressBook.removeBuddy(0).getName());
     }
 }
